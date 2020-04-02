44e7ad6c8347036406141e7dab7a080394ea5577
kenglbre <kevin.englbrecht@hm.edu>
2020-04-02 23:37:15 +0200

    +Methode (containsAllIgnoreCase) +Methode erweiterung (shouldSearchStrings(Junittest))

---
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+import static java.util.Arrays.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+package edu.hm.hafner.util;
+
+public class SetTest {
+
+@test
+    void
+}
